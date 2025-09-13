dda2243e50dee554cb496668d693d9b9bc4c7f3e
deralph <jraphael441@gmail.com>
2025-09-13 10:07:34 +0100

    changed server url

--- services/api.ts
+++ services/api.ts
@@ -4,7 +4,7 @@ import { getToken,removeToken } from '../utils/tokenStorage';
 
 // npm install --global eas-cli && eas init --id aac28bfa-9fec-43d5-a7d1-3ef7380f63bb
 // https://gate-pass-backend-yz1r.onrender.com
-const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.55.193:5000/api';
+const API_BASE_URL = "https://gate-pass-backend-yz1r.onrender.com" || 'http://192.168.55.193:5000/api';
  
 const api = axios.create({
   baseURL: API_BASE_URL,
